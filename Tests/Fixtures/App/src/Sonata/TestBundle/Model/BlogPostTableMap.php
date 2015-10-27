eda4ab01514d05de57ae35a4fbc0195cbe1e8d78
Marcin Chylek <marcin@chylek.pl>
2015-10-27 09:52:45 +0100

    Refactoring propel2

---
+++ Tests/Fixtures/App/src/Sonata/TestBundle/Model/BlogPostTableMap.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Sonata\TestBundle\Model;
+
+use Sonata\TestBundle\Model\Map\BlogPostTableMap as BaseBlogPostTableMap;
+
+class BlogPostTableMap extends BaseBlogPostTableMap
+{
+
+}
